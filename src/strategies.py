16e9506c65eff298b5a1185bb443413a5b922427
Matthew Boniface <mboniface5@yahoo.com>
2026-06-22 15:57:26 +0100

    Create the class for the mean reversion strategy

--- src/strategies.py
+++ src/strategies.py
@@ -16,4 +16,18 @@ class MomentumStrategy():
         returns = data["Close"].pct_change(self.lookback)
         
         signals = (returns > 0).astype(int)
-        return signals.fillna(0)    
+        return signals.fillna(0)
+    
+class MeanReversionStrategy():
+    def __init__(self,lookback: int = 20, num_std: int = 2):
+        self.lookback = lookback
+        self.num_std = num_std
+        
+    def generate_signals(self, data: pd.DataFrame) -> pd.Series:
+        mean = data.rolling(self.lookback).mean()
+        std = data.rolling(self.lookback).std()
+        
+        lower_band = mean - (std* self.num_std)
+        
+        signals = (data["Close"] < lower_band).astype(int)
+        return signals.fillna(0)
