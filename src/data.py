6c310ead653a72732b20543ae4a840d6e8665d51
Matthew Boniface <mboniface5@yahoo.com>
2026-06-22 14:20:19 +0100

    Add data.py file which extracts price history for any 1 ticker

---
+++ src/data.py
@@ -0,0 +1,23 @@
+import yfinance as yf
+from pathlib import Path
+import pandas as pd
+
+class DataLoader():
+    def get_price_history(self,ticker: str,start: str,end: str) -> pd.DataFrame:
+        yf_ticker = yf.Ticker(ticker)
+        data = yf_ticker.history(start=start,
+                               end=end
+                               )
+        if data.empty:
+            raise ValueError(f"No data returned for {ticker} between {start} and {end}")
+
+        data = pd.DataFrame(data)
+        file_path = f"{Path('data')}/{ticker}_{start}-{end}.csv"
+        data.to_csv(file_path,)
+        
+        return data
+        
+        
+loader = DataLoader()
+data = loader.get_price_history("AAPL","2026-01-01","2026-01-10")
+print(data)
